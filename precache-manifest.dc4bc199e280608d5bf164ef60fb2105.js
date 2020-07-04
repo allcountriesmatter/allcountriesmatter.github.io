ff81246b03b5a4a36aab0225f914d20d156dcc82
Colby Butler <colby@owljet.com>
2020-07-04 10:55:00 -0400

    1.0

---
+++ precache-manifest.dc4bc199e280608d5bf164ef60fb2105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe05bbe5e2d23336227c2094ade6121",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b3190ff32609fd25de0",
+    "url": "/static/css/main.ed16481d.chunk.css"
+  },
+  {
+    "revision": "8bf3bb2c9560ad1ec128",
+    "url": "/static/js/2.ae0575b5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ae0575b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3190ff32609fd25de0",
+    "url": "/static/js/main.d20a429f.chunk.js"
+  },
+  {
+    "revision": "b3c1433566e4d341b3a8",
+    "url": "/static/js/runtime-main.2ba3fec9.js"
+  }
+]);
